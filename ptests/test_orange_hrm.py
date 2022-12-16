82e4b1d83bf8306a9127296aae4dfea9bc379066
SalvaVamsiKasyap <SalvaVamsiKasyap@gmail.com>
2022-12-16 16:16:36 +0530

    Pushing the log

--- ptests/test_orange_hrm.py
+++ ptests/test_orange_hrm.py
@@ -104,67 +104,11 @@ from utilities.BaseClass import BaseClass
 #         #log.info("Verified..")
 
 
-@pytest.mark.usefixtures('setup')
-class Testorange_hrm_edit_user_in_pim(BaseClass):
-
-    def test_edituser(self):
-
-        log = self.get_logger()
-        ohlp = login_Page(self.driver)
-        self.verify_element_presence('//input[@name="username"]')
-        username_inp = ohlp.enter_user_name()
-        log.info("Entering the username..")
-        username_inp.send_keys('Admin')
-        log.info("Entered user name as Admin.")
-        userpassword_inp = ohlp.enter_user_password()
-        log.info("Entering the password")
-        userpassword_inp.send_keys('admin123')
-        log.info("Entered the password..")
-        userpassword_inp.send_keys(Keys.ENTER)
-        hmp = home_Page(self.driver)
-        self.verify_element_presence('//a[@href="/web/index.php/pim/viewPimModule"]')
-        log.info("Clicking on PIM Module..")
-        hmp.selecting_Pim_Module()
-        log.info("Successfully Clicked PIM Module")
-        pim_M_P = pim_Module_Page(self.driver)
-        self.verify_element_presence("//i[@class='oxd-icon bi-pencil-fill']//parent::button")
-        log.info("Clicking on edit button..")
-        pim_M_P.editing_existing_employee_details()
-        log.info("Successfully clicked edit button")
-        self.verify_element_presence("//input[@name='firstName']")
-        fris_nam = pim_M_P.entering_First_Name()
-        fris_nam.clear()
-        fris_nam.send_keys("salva")
-        log.info("Entered the first name as a part of editing..")
-        self.verify_element_presence("//input[@name='middleName']")
-        middl_nam = pim_M_P.entering_Middle_Name()
-        log.info("Entering the middle name as a part of editing..")
-        middl_nam.clear()
-        middl_nam.send_keys('gandhi')
-        log.info("Entered the middlename name as a part of editing..")
-        self.verify_element_presence("//input[@name='lastName']")
-        las_nam = pim_M_P.entering_Last_Name()
-        log.info("Entering the last name as a part of editing..")
-        las_nam.clear()
-        las_nam.send_keys("damodara")
-        log.info("Entered the last name as a part of editing..")
-        self.verify_element_presence('//label[text()="Employee Id"]//parent::div//following-sibling::div/input')
-        employe_id = pim_M_P.entering_Employee_Id()
-        log.info("Entering the employee id as a part of editing..")
-        employe_id.send_keys("3456")
-        #employe_id.send_keys(Keys.ENTER)
-        log.info("Entered the employee id as a part of editing..")
-        self.verify_element_presence('//label[text()="Blood Type"]//parent::div//parent::div//parent::div//parent::div//parent::div//parent::form/div/button')
-        edit = pim_M_P.saving_post_editing()
-        log.info(self.verify_element_presence('//p[text()="Successfully Saved"]'))
-        #log.info(f"Editing operation successful {edit}")
-
-
 # @pytest.mark.usefixtures('setup')
-# class Testorange_hrm_delte_user_in_pim(BaseClass):
+# class Testorange_hrm_edit_user_in_pim(BaseClass):
 #
-#     def test_deleteuser(self):
-#
+#     def test_edituser(self):
+#         desired_Message = "Successfully Saved"
 #         log = self.get_logger()
 #         ohlp = login_Page(self.driver)
 #         self.verify_element_presence('//input[@name="username"]')
@@ -183,10 +127,70 @@ class Testorange_hrm_edit_user_in_pim(BaseClass):
 #         hmp.selecting_Pim_Module()
 #         log.info("Successfully Clicked PIM Module")
 #         pim_M_P = pim_Module_Page(self.driver)
-#         log.info("Clicking on delete icon..")
-#         self.verify_element_presence("//i[@class='oxd-icon bi-trash']//parent::button")
-#         pim_M_P.delete_existing_employee_details()
-#         log.info(self.verify_element_presence("//p[text()='Successfully Deleted']"))
+#         self.verify_element_presence("//i[@class='oxd-icon bi-pencil-fill']//parent::button")
+#         log.info("Clicking on edit button..")
+#         pim_M_P.editing_existing_employee_details()
+#         log.info("Successfully clicked edit button")
+#         self.verify_element_presence("//input[@name='firstName']")
+#         fris_nam = pim_M_P.entering_First_Name()
+#         fris_nam.clear()
+#         fris_nam.send_keys("salva")
+#         log.info("Entered the first name as a part of editing..")
+#         self.verify_element_presence("//input[@name='middleName']")
+#         middl_nam = pim_M_P.entering_Middle_Name()
+#         log.info("Entering the middle name as a part of editing..")
+#         middl_nam.clear()
+#         middl_nam.send_keys('gandhi')
+#         log.info("Entered the middlename name as a part of editing..")
+#         self.verify_element_presence("//input[@name='lastName']")
+#         las_nam = pim_M_P.entering_Last_Name()
+#         log.info("Entering the last name as a part of editing..")
+#         las_nam.clear()
+#         las_nam.send_keys("damodara")
+#         log.info("Entered the last name as a part of editing..")
+#         self.verify_element_presence('//label[text()="Employee Id"]//parent::div//following-sibling::div/input')
+#         employe_id = pim_M_P.entering_Employee_Id()
+#         log.info("Entering the employee id as a part of editing..")
+#         employe_id.send_keys("3456")
+#         #employe_id.send_keys(Keys.ENTER)
+#         log.info("Entered the employee id as a part of editing..")
+#         self.verify_element_presence('//label[text()="Blood Type"]//parent::div//parent::div//parent::div//parent::div//parent::div//parent::form/div/button')
+#         edit = pim_M_P.saving_post_editing()
+#         log.info(self.verify_element_presence('//p[text()="Successfully Saved"]'))
+#         assert self.verify_element_presence('//p[text()="Successfully Saved"]') == desired_Message
+#
+#         #log.info(f"Editing operation successful {edit}")
+
+
+@pytest.mark.usefixtures('setup')
+class Testorange_hrm_delte_user_in_pim(BaseClass):
+
+    def test_deleteuser(self):
+
+        desired_Message = "Successfully Deleted"
+        log = self.get_logger()
+        ohlp = login_Page(self.driver)
+        self.verify_element_presence('//input[@name="username"]')
+        username_inp = ohlp.enter_user_name()
+        log.info("Entering the username..")
+        username_inp.send_keys('Admin')
+        log.info("Entered user name as Admin.")
+        userpassword_inp = ohlp.enter_user_password()
+        log.info("Entering the password")
+        userpassword_inp.send_keys('admin123')
+        log.info("Entered the password..")
+        userpassword_inp.send_keys(Keys.ENTER)
+        hmp = home_Page(self.driver)
+        self.verify_element_presence('//a[@href="/web/index.php/pim/viewPimModule"]')
+        log.info("Clicking on PIM Module..")
+        hmp.selecting_Pim_Module()
+        log.info("Successfully Clicked PIM Module")
+        pim_M_P = pim_Module_Page(self.driver)
+        log.info("Clicking on delete icon..")
+        self.verify_element_presence("//i[@class='oxd-icon bi-trash']//parent::button")
+        pim_M_P.delete_existing_employee_details()
+        log.info(self.verify_element_presence("//p[text()='Successfully Deleted']"))
+        assert self.verify_element_presence('//p[text()="Successfully Deleted"]') == desired_Message
 
 
 
